55701d6d263a67679322da669bed7c81bed04018
alexmortelsison <alexmortelsison@gmail.com>
2024-04-04 08:21:23 +0900

    Initial Commit

---
+++ TipSplitter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipSplitter
+//
+//  Created by Alex Sison on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TipSplitter/TipSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipSplitterApp.swift
+//  TipSplitter
+//
+//  Created by Alex Sison on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TipSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
