1be113b53cff1ab2f1be890ad1696f586473227c
alexmortelsison <alexmortelsison@gmail.com>
2024-04-04 08:25:43 +0900

    initial commit

--- TipSplitter/ContentView.swift
+++ TipSplitter/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 10
+    
+    let percentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: "JPY"))
+            }
         }
-        .padding()
     }
 }
 
